104215ada4cf5eabf7fbe07d4582cdaf7f02808d
gurezo <akihiko.kigure@gmail.com>
2018-07-23 21:28:00 +0900

    toastr isn't used. it is bostugit add .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { toastr } from '../../node_modules/toastr/build/toastr.min.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  onClick() {
+    // Display an info toast with no title
+    // toastr.info('Are you the 6 fingered man?');
+    // 手間がかかる事を確認。ボツ。
+  }
 }
